SameInit + strtified clients + data leakage fixed

models/autoencoder.py
@@ -35,7 +35,7 @@ class AutoEncoder(nn.Module):
 
     def init_weights(self, m):
         if type(m) in [nn.Linear]:
-            torch.nn.init.kaiming_normal_(m.weight, nonlinearity='relu')
+            torch.nn.init.kaiming_normal_(m.weight, nonlinearity='relu') # ugyanaz az init!!
             m.bias.data.fill_(0.01)
 
 
@@ -94,7 +94,7 @@ def validate_ae(model_cfg, data, kwargs):
             y_hat_list.append(output)
             output_sorted = output.sort().values
             th_value = output_sorted[int(output.shape[0] * kwargs['anomaly_trhold']) - 1].item()
-            pred_list.append(torch.where(output >= th_value, 1.0, 0.0))
+            pred_list.append(torch.where(output > th_value, 1.0, 0.0))
 
         y_true = torch.cat(y_true_list, dim=0).cpu().numpy()
         y_hat = torch.cat(y_hat_list, dim=0).cpu().numpy()
@@ -125,7 +125,7 @@ def predict_ae(model_cfg, data, kwargs):
             output = loss(x_hat, X).mean(dim = 1).view(-1)
             output_sorted = output.sort().values
             th_value = output_sorted[int(output.shape[0] * kwargs['anomaly_trhold']) - 1].item()
-            preds.append(torch.where(output >= th_value, 1.0, 0.0))
+            preds.append(torch.where(output > th_value, 1.0, 0.0))
 
         preds = torch.cat(preds, dim=0).cpu().numpy()
 

utils/data.py
@@ -1,12 +1,14 @@
 from shuttle.data import load_shuttle, ShuttleDataset
 from sklearn.utils import shuffle
 from sklearn.model_selection import train_test_split
+from sklearn.model_selection import StratifiedKFold
 from torch.utils.data import DataLoader
+import numpy as np
 
 datasets = {'shuttle': ShuttleDataset}
 loader_func = {'shuttle': load_shuttle}
 
-def data_loaders(models_cfg, valid_split = 0.1, test_split = 0.1, random_state = 42):
+def data_loaders(models_cfg, valid_split = 0.1, test_split = 0.1, random_state = 42, stratified_clients = False):
 
     print("Loading local datasets..")
     load_func = loader_func.get(models_cfg['dataset'])
@@ -22,19 +24,36 @@ def data_loaders(models_cfg, valid_split = 0.1, test_split = 0.1, random_state =
 
     x_shuffled, y_shuffled = shuffle(X, Y, random_state=random_state)
     x_tv, x_test, y_tv, y_test = train_test_split(
-        x_shuffled, y_shuffled, test_size=test_split, random_state=random_state)
+        x_shuffled, y_shuffled, test_size=test_split, random_state=random_state, stratify=y_shuffled)
     x_train, x_valid, y_train, y_valid = train_test_split(
-        x_tv, y_tv, test_size=valid_split, random_state=random_state)
+        x_tv, y_tv, test_size=valid_split, random_state=random_state, stratify=y_tv)
+
+    # standard scaling (no data leakage)
+    mean = np.mean(x_train)
+    std = np.std(x_train)
+    x_train = (x_train - mean) / std
+    x_valid = (x_valid - mean) / std
+    x_test = (x_test - mean) / std
+
+    # x_train = x_train[np.where(y_train.flatten() == 0.0)]
+    # y_train = y_train[np.where(y_train.flatten() == 0.0)]
 
     models_cfg['models'] = []
+    sss = StratifiedKFold(n_splits=models_cfg['nr_models'])
     data_len = x_train.shape[0] // models_cfg['nr_models']
 
-    for i in range(models_cfg['nr_models']):
+    for i, indexes in enumerate(sss.split(x_train, y_train)):
         models_cfg['models'].append({})
         models_cfg['models'][i]['loaders'] = {}
 
-        m_data_x = x_train[i*data_len : (i + 1)*data_len]
-        m_data_y = y_train[i*data_len : (i + 1)*data_len]
+        if stratified_clients:
+            #stratified split for each client
+            _, m_index = indexes
+            m_data_x = x_train[m_index]
+            m_data_y = y_train[m_index]
+        else:
+            m_data_x = x_train[i*data_len : (i + 1)*data_len]
+            m_data_y = y_train[i*data_len : (i + 1)*data_len]
 
         dataset = datasets[models_cfg['dataset']](x=m_data_x, y=m_data_y)
         loader = DataLoader(dataset, batch_size=models_cfg['batch_size'], pin_memory=True)

utils/models.py
@@ -9,6 +9,7 @@ optimizers = {
 
 losses = {
     'L1Loss': torch.nn.L1Loss,
+    'MSELoss': torch.nn.MSELoss,
     'BCE': torch.nn.BCELoss
 }
 
@@ -29,10 +30,14 @@ def build_models(models_cfg):
         m['net'] = model_cls(models_cfg['model']).to(m['dev'])
 
         if 'optimizer' in models_cfg['model']:
-
-            m['net'].apply(m['net'].init_weights)
+            if i == 0:
+                m['net'].apply(m['net'].init_weights)
+            else:
+                m['net'].load_state_dict(models_cfg['models'][0]['net'].state_dict())
             m['optimizer'] = optimizers[models_cfg['model']['optimizer']](m['net'].parameters(), lr=models_cfg['model']['lr'])
             m['loss'] = losses[models_cfg['model']['loss']](reduction='none')
+
+            #print(models_cfg['models'][i]['net'].state_dict()['net.enc_1.0.weight'])
 
     return models_cfg
 